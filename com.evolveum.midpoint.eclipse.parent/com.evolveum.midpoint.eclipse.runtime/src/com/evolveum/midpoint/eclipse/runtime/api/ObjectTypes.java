f66d3779be1a6242cb86a1fb119d7feda9f8aac7
Pavol Mederly <mederly@evolveum.com>
2019-05-02 18:36:27 +0200

    Add object types for midPoint 4.0 (MID-5177)
    Note that support for assignmentHolder is missing.

--- com.evolveum.midpoint.eclipse.parent/com.evolveum.midpoint.eclipse.runtime/src/com/evolveum/midpoint/eclipse/runtime/api/ObjectTypes.java
+++ com.evolveum.midpoint.eclipse.parent/com.evolveum.midpoint.eclipse.runtime/src/com/evolveum/midpoint/eclipse/runtime/api/ObjectTypes.java
@@ -9,32 +9,40 @@ import javax.xml.namespace.QName;
 
 public enum ObjectTypes {
 	
-	OBJECT("object", "objects", "ObjectType", "Object (abstract type)", null, null, null),
-    FOCUS("focus", "focus", "FocusType", "Focus (abstract type)", null, null, OBJECT),
-    ABSTRACT_ROLE("abstractRole", "abstractRoles", "AbstractRoleType", "Abstract role (abstract type)", null, "displayName", FOCUS),
-    CONNECTOR("connector", "connectors", "ConnectorType", "Connector", "connectorType", "displayName", OBJECT),
-    CONNECTOR_HOST("connectorHost", "connectorHosts", "ConnectorHostType", "Connector host", null, null, OBJECT),
-    GENERIC_OBJECT("genericObject", "genericObjects", "GenericObjectType", "Generic object", null, null, OBJECT),
-    RESOURCE("resource", "resources", "ResourceType", "Resource", null, null, OBJECT),
-    USER("user", "users", "UserType", "User", "employeeType", "fullName", FOCUS),
-    OBJECT_TEMPLATE("objectTemplate", "objectTemplates", "ObjectTemplateType", "Object template", null, null, OBJECT),
-    SYSTEM_CONFIGURATION("systemConfiguration", "systemConfigurations", "SystemConfigurationType", "System configuration", null, null, OBJECT),
-    TASK("task", "tasks", "TaskType", "Task", "category", null, OBJECT),
-    SHADOW("shadow", "shadows", "ShadowType", "Shadow", null, null, OBJECT),
-    ROLE("role", "roles", "RoleType", "Role", "roleType", "displayName", ABSTRACT_ROLE),
-    PASSWORD_POLICY("valuePolicy", "valuePolicies", "ValuePolicyType", "Value policy", null, null, OBJECT),
-    NODE("node", "nodes", "NodeType", "Node", null, null, OBJECT),
-    FORM("form", "forms", "FormType", "Form", null, null, OBJECT),
-    ORG("org", "orgs", "OrgType", "Organization", "orgType", "displayName", ABSTRACT_ROLE),
-    REPORT("report", "reports", "ReportType", "Report", null, null, OBJECT),
-    REPORT_OUTPUT("reportOutput", "reportOutputs", "ReportOutputType", "Report output", null, null, OBJECT),
-    SECURITY_POLICY("securityPolicy", "securityPolicies", "SecurityPolicyType", "Security policy", null, null, OBJECT),
-    LOOKUP_TABLE("lookupTable", "lookupTables", "LookupTableType", "Lookup table", null, null, OBJECT),
-    ACCESS_CERTIFICATION_DEFINITION("accessCertificationDefinition", "accessCertificationDefinitions", "AccessCertificationDefinitionType", "Access certification definition", null, null, OBJECT),
-    ACCESS_CERTIFICATION_CAMPAIGN("accessCertificationCampaign", "accessCertificationCampaigns", "AccessCertificationCampaignType", "Access certification campaign", null, null, OBJECT),
-    SEQUENCE("sequence", "sequences", "SequenceType", "Sequence", null, null, OBJECT),
-    SERVICE("service", "services", "ServiceType", "Service", "serviceType", "displayName", ABSTRACT_ROLE),
-    FUNCTION_LIBRARY("functionLibrary", "functionLibraries", "FunctionLibraryType", "Function library", null, null, OBJECT),
+	// TODO implement AssignmentHolderType correctly here - i.e. as part of the inheritance hierarchy 
+	// But be careful: it is known only since midPoint 4.0
+	
+	OBJECT("object", "objects", "ObjectType", "Object (abstract type)", null, null, null, false),
+	ASSIGNMENT_HOLDER("assignmentHolder", "assignmentHolders", "AssignmentHolderType", "Assignment holder (abstract type)", null, null, OBJECT, false),
+    FOCUS("focus", "focus", "FocusType", "Focus (abstract type)", null, null, OBJECT, false),
+    ABSTRACT_ROLE("abstractRole", "abstractRoles", "AbstractRoleType", "Abstract role (abstract type)", null, "displayName", FOCUS, false),
+    CONNECTOR("connector", "connectors", "ConnectorType", "Connector", "connectorType", "displayName", OBJECT, true),
+    CONNECTOR_HOST("connectorHost", "connectorHosts", "ConnectorHostType", "Connector host", null, null, OBJECT, true),
+    GENERIC_OBJECT("genericObject", "genericObjects", "GenericObjectType", "Generic object", null, null, OBJECT, true),
+    RESOURCE("resource", "resources", "ResourceType", "Resource", null, null, OBJECT, true),
+    USER("user", "users", "UserType", "User", "employeeType", "fullName", FOCUS, true),
+    OBJECT_TEMPLATE("objectTemplate", "objectTemplates", "ObjectTemplateType", "Object template", null, null, OBJECT, true),
+    SYSTEM_CONFIGURATION("systemConfiguration", "systemConfigurations", "SystemConfigurationType", "System configuration", null, null, OBJECT, true),
+    TASK("task", "tasks", "TaskType", "Task", "category", null, OBJECT, true),
+    SHADOW("shadow", "shadows", "ShadowType", "Shadow", null, null, OBJECT, true),
+    ROLE("role", "roles", "RoleType", "Role", "roleType", "displayName", ABSTRACT_ROLE, true),
+    PASSWORD_POLICY("valuePolicy", "valuePolicies", "ValuePolicyType", "Value policy", null, null, OBJECT, true),
+    NODE("node", "nodes", "NodeType", "Node", null, null, OBJECT, true),
+    FORM("form", "forms", "FormType", "Form", null, null, OBJECT, true),
+    ORG("org", "orgs", "OrgType", "Organization", "orgType", "displayName", ABSTRACT_ROLE, true),
+    REPORT("report", "reports", "ReportType", "Report", null, null, OBJECT, true),
+    REPORT_OUTPUT("reportOutput", "reportOutputs", "ReportOutputType", "Report output", null, null, OBJECT, true),
+    SECURITY_POLICY("securityPolicy", "securityPolicies", "SecurityPolicyType", "Security policy", null, null, OBJECT, true),
+    LOOKUP_TABLE("lookupTable", "lookupTables", "LookupTableType", "Lookup table", null, null, OBJECT, true),
+    ACCESS_CERTIFICATION_DEFINITION("accessCertificationDefinition", "accessCertificationDefinitions", "AccessCertificationDefinitionType", "Access certification definition", null, null, OBJECT, true),
+    ACCESS_CERTIFICATION_CAMPAIGN("accessCertificationCampaign", "accessCertificationCampaigns", "AccessCertificationCampaignType", "Access certification campaign", null, null, OBJECT, true),
+    SEQUENCE("sequence", "sequences", "SequenceType", "Sequence", null, null, OBJECT, true),
+    SERVICE("service", "services", "ServiceType", "Service", "serviceType", "displayName", ABSTRACT_ROLE, true),
+    FUNCTION_LIBRARY("functionLibrary", "functionLibraries", "FunctionLibraryType", "Function library", null, null, OBJECT, true),
+    ARCHETYPE("archetype", "archetypes", "ArchetypeType", "Archetype", null, null, ABSTRACT_ROLE, true),
+    CASE("case", "cases", "CaseType", "Case", null, null, OBJECT, true),
+    OBJECT_COLLECTION("objectCollection", "objectCollections", "ObjectCollectionType", "Object collection", null, null, OBJECT, true),
+    DASHBOARD("dashboard", "dashboards", "DashboardType", "Dashboard", null, null, OBJECT, true)
     ;
 	
 	private String elementName;
@@ -44,8 +52,9 @@ public enum ObjectTypes {
 	private String subTypeElement;
 	private String displayNameElement;
 	private ObjectTypes superType;
+	private boolean concrete;
 	
-	ObjectTypes(String elementName, String restType, String typeName, String displayName, String subTypeElement, String displayNameElement, ObjectTypes superType) {
+	ObjectTypes(String elementName, String restType, String typeName, String displayName, String subTypeElement, String displayNameElement, ObjectTypes superType, boolean concrete) {
 		this.elementName = elementName;
 		this.restType = restType;
 		this.typeName = typeName;
@@ -53,6 +62,7 @@ public enum ObjectTypes {
 		this.subTypeElement = subTypeElement;
 		this.displayNameElement = displayNameElement;
 		this.superType = superType;
+		this.concrete = concrete;
 	}
 	
     public String getElementName() {
@@ -76,12 +86,7 @@ public enum ObjectTypes {
 	}
 
 	public boolean isConcrete() {
-		for (ObjectTypes t : values()) {
-			if (t.superType == this) {
-				return false;
-			}
-		}
-		return true;
+		return concrete;
 	}
 
 	public static String getRestTypeForElementName(String elementName) {
